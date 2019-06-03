d61e0aeba3c756749d6aa79f4b2768c59b17eeeb
vianat <vianatdev@gmail.com>
2019-06-02 20:22:09 -0500

    add screenshot tests

---
+++ selectors/screenshots.js
@@ -0,0 +1,54 @@
+export default {
+
+    lnkSpain: '#lnk_Spanish',
+    lnkEnglish: '#lnk_English',
+
+    header: ".header-container",
+
+    banner: ".banner",
+    img1: ".section1 img",
+    img2: ".section2 img",
+    img3: ".section3 img",
+
+    otherOptions: ".section-panel",
+    questions: ".section-panel1",
+    languages: ".announcement",
+    warning: ".warningannouncement",
+    logo: "#div_Arizona_Logo",
+    footer: ".footer",
+
+    return: "#lnk_ReturningUserLogIn",
+    returnPopUp: "#div_login",
+
+    help: "#lnk_Help",
+    faq: "#lnk_faqs",
+    find: "#lnk_ClickHereIf",
+    why: "#lnk_HEAplus",
+    privacy: "#lnk_Privacy",
+    privacySP: "#lnk_Privacy_ES",
+    mission: "#lnk_Mission",
+    nonDiscrim: "#lnk_NonDiscrimination",
+    about: "#lnk_AboutHEAplus",
+    faqFooter: "#lnk_end",
+    popUp: "#dynDiv",
+    popUpClose: "span.ui-icon-closethick",
+
+    langDescription: "#Div_Other_Lang_Text",
+
+    spain: '#Hlnk_Spanish',
+    arabic: '#Hlnk_Arabic',
+    german: '#Hlnk_German',
+    syriac: '#HyperLink1',
+    navajo: '#Hlnk_Navajo',
+    tagalog: '#Hlnk_Tagalog',
+    russian: '#Hlnk_Russian',
+    serbian: '#HyperLink2',
+    chinese: '#Hlnk_Chinese',
+    korean: '#Hlnk_Korean',
+    japanese: '#Hlnk_Japanese',
+    thai: '#HyperLink3',
+    vietnamese: '#Hlnk_Vietnamese',
+    french: '#Hlnk_French',
+    persian: '#Hlnk_Persian',
+    italian: '#HyperLink4'
+}
---
+++ test/data/screenshots.js
@@ -0,0 +1,18 @@
+export default {
+    screenPath: './screenshots',
+    basePath: '/baseline/',
+    actPath: '/actual/',
+    diffPath: '/diff/',
+    mergePath: './merge/',
+    login: 'login',
+    forgot: 'forgot',
+    register: 'registration',
+    listHeader: 'bug-list-header',
+    listNavBar: 'bug-list-nav-bar',
+    listTHead: 'bug-list-thead',
+    listLastBug: 'bug-list-last-bug',
+    listFooter: 'bug-list-footer',
+    bugDefault: 'bug-report-default-mode',
+    bugEdit: 'bug-report-edit-mode'
+
+}
